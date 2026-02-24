962b32c34a94f645be29ca7e8423213ed6eca7c8
vipulvanne3-23 <vipulvanne3@gmail.com>
2026-02-24 09:26:38 +0530

    Update Main1.py

--- Main1.py
+++ Main1.py
@@ -2,4 +2,4 @@ def rectangle_area(length, width):
     return length * width
 
 if __name__ == '__main__':
-    print(f"Rectangle area (5x3): {rectangle_area(5, 3)}")
+    print(f"Rectangle area (5x3): {rectangle_area(5, 5)}")
