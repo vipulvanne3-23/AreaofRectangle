95173ba9bdbc5747d671d4146991fd32fb314cf5
vipulvanne3-23 <vipulvanne3@gmail.com>
2026-02-24 09:24:32 +0530

    Update test.py

--- test.py
+++ test.py
@@ -2,8 +2,7 @@ python
 from Main1 import rectangle_area
 
 def test_rectangle_area():
-    assert rectangle_area(5, 3) == 15, "Area of rectangle with length 5 and width 3 should be 15"
-
+    assert rectangle_area(5, 3) == 14, "Area of rectangle with length 5 and width 3 should be 15"
 if __name__ == '__main__':
     test_rectangle_area()
     print("✓ Test passed!")
